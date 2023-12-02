06095398be898fcc4078204a9cb8c37de4a07072
alexeymametyev <mametevalex@gmail.com>
2023-12-02 18:33:14 +0300

    [FIX] if history is None

--- history.py
+++ history.py
@@ -12,6 +12,11 @@ class DialogHistory:
             :param system_message: system message to start the dialog with
                 """
         self._history: List[BaseMessage] = []
+        if not history:
+            if system_message:
+                self._history = [SystemMessage(content=system_message)]
+            else:
+                self._history = []
         if isinstance(history[0], tuple):
             for user_message, ai_message in history:
                 self._history.append(HumanMessage(content=user_message))
@@ -33,14 +38,24 @@ class DialogHistory:
                     self._history.append(AIMessage(content=message))
                     message_source = 'user'
 
-        self._system_message = system_message
+        if not system_message and isinstance(history[0], SystemMessage):
+            self._system_message = history[0].content
+        else:
+            self._system_message = system_message
         if not isinstance(self._history[0], SystemMessage) and system_message:
             self._history = [SystemMessage(content=system_message)] + self._history
 
     @property
     def gradio_history(self) -> List[tuple]:
-        return [(user_message.content, ai_message.content) for user_message, ai_message in
-                zip(self._history[::2], self._history[1::2])]
+        res = []
+        for message in self._history:
+            if isinstance(message, HumanMessage):
+                res.append((message.content, None))
+            elif isinstance(message, AIMessage):
+                res[-1] = (res[-1][0], message.content)
+        if res[-1][1] is None:
+            res.pop()
+        return res
 
     @property
     def long_chain_history(self):
