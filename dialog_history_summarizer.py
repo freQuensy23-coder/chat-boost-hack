66661693f2bbc7139d34bf2521fe2075efb5787b
alexeymametyev <mametevalex@gmail.com>
2023-12-02 13:51:02 +0300

    [IMP] dialog summarizer

--- dialog_history_summarizer.py
+++ dialog_history_summarizer.py
@@ -1,6 +1,9 @@
 from langchain.chat_models import GigaChat
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 import config
+from history import DialogHistory
+from utils import batched
 
 
 class DialogHistorySummarizer:
@@ -17,4 +20,19 @@ class DialogHistorySummarizer:
     def __init__(self):
         self.llm = GigaChat(credentials=config.token, verify_ssl_certs=False)
 
-    def __call__(self, ):
+    def __call__(self, history: DialogHistory) -> str:
+        system_message = """Тебе дан диалог между пользователем и менеджером. Переформулируй его в один понятный вопрос от пользователя, начни с User:"""
+        prompt = [SystemMessage(content=system_message), HumanMessage(content=history.__str__())]
+        res = self.llm(prompt)
+        return res.content
+
+
+if __name__ == '__main__':
+    while True:
+        input_text = input('Ввод в формате фразы через \\n')
+        history = input_text.split('\n')
+
+        history = DialogHistory(*batched(history, 2), system_message=None)
+        summarizer = DialogHistorySummarizer()
+        print(summarizer(history))
+
--- history.py
+++ history.py
@@ -4,33 +4,43 @@ from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, Base
 
 
 class DialogHistory:
-    def __init__(self, history, system_message: str | None):
+    def __init__(self, history, system_message: str | None = None):
         """:param history: in any format - gradio or long chain
                 [(user_message, bot_message), ...]
                 or
                 [HumanMessage(content=user_message), AIMessage(content=bot_message), ...]
             :param system_message: system message to start the dialog with
                 """
-        self._history : List[BaseMessage]
+        self._history: List[BaseMessage] = []
         if isinstance(history[0], tuple):
-            self._history = [SystemMessage(content=system_message)]
-
             for user_message, ai_message in history:
                 self._history.append(HumanMessage(content=user_message))
                 self._history.append(AIMessage(content=ai_message))
+
         elif isinstance(history[0], BaseMessage):
-            if isinstance(history[0], HumanMessage):
-                history = [SystemMessage(content=system_message)] + history
-            if isinstance(history[0], SystemMessage):
+            if isinstance(history[0], HumanMessage) or isinstance(history[0], SystemMessage):
                 self._history = history
             if isinstance(history[0], AIMessage):
                 raise ValueError("First message in history can't be AIMessage")
 
+        elif isinstance(history[0], str):
+            message_source = 'user'
+            for message in history:
+                if message_source == 'user':
+                    self._history.append(HumanMessage(content=message))
+                    message_source = 'ai'
+                else:
+                    self._history.append(AIMessage(content=message))
+                    message_source = 'user'
+
         self._system_message = system_message
+        if not isinstance(self._history[0], SystemMessage) and system_message:
+            self._history = [SystemMessage(content=system_message)] + self._history
 
     @property
-    def gradio_history(self)-> List[tuple]:
-        return [(user_message.content, ai_message.content) for user_message, ai_message in zip(self._history[::2], self._history[1::2])]
+    def gradio_history(self) -> List[tuple]:
+        return [(user_message.content, ai_message.content) for user_message, ai_message in
+                zip(self._history[::2], self._history[1::2])]
 
     @property
     def long_chain_history(self):
@@ -45,6 +55,20 @@ class DialogHistory:
         if bot_message:
             self._history.append(AIMessage(content=bot_message))
 
+    def __str__(self):
+        """Return User: message \n AI: message \n ..."""
+        source = 'user'
+        res = ''
+        for message in self._history:
+            if source == 'user':
+                res += f'User: {message.content}\n'
+                source = 'ai'
+            else:
+                res += f'AI: {message.content}\n'
+                source = 'user'
+
+        return res.strip()
+
 
 if __name__ == '__main__':
     history = DialogHistory([('Привет', 'Привет'), ('Как дела?', 'Нормально'), ('Чем занимаешься?', 'Пишу код')],
@@ -58,3 +82,8 @@ if __name__ == '__main__':
     history.add_message('Чем занимаешься?', 'Пишу код')
     print(history.gradio_history)
     print(history.long_chain_history)
+
+    history2 = DialogHistory(['Привет'])
+    print(history2.gradio_history)
+    print(history2.long_chain_history)
+    print(str(history2))
